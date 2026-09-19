31d7aeeb9b029a35635417cf2280a2f2253e4ddc
Shiney-X <romulohenrique355@gmail.com>
2026-09-19 17:21:20 -0300

    ci: align Xbox overlay with upstream checks

--- platform/xbox_uwp/d3d12_status_renderer.h
+++ platform/xbox_uwp/d3d12_status_renderer.h
@@ -10,31 +10,32 @@
 
 class D3D12StatusRenderer final {
 public:
-    ~D3D12StatusRenderer();
+  ~D3D12StatusRenderer();
 
-    void Initialize(IUnknown* core_window, float width, float height);
-    void Render(bool passed);
-    [[nodiscard]] bool TryTrim();
+  void Initialize(IUnknown *core_window, float width, float height);
+  void Render(bool passed);
+  [[nodiscard]] bool TryTrim();
 
 private:
-    void CreateTrianglePipeline();
-    void WaitForGpu();
+  void CreateTrianglePipeline();
+  void WaitForGpu();
 
-    static constexpr UINT FrameCount = 2;
+  static constexpr UINT FrameCount = 2;
 
-    Microsoft::WRL::ComPtr<ID3D12Device> device_;
-    Microsoft::WRL::ComPtr<ID3D12CommandQueue> command_queue_;
-    Microsoft::WRL::ComPtr<IDXGISwapChain3> swap_chain_;
-    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtv_heap_;
-    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> command_allocator_;
-    Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> command_list_;
-    Microsoft::WRL::ComPtr<ID3D12RootSignature> root_signature_;
-    Microsoft::WRL::ComPtr<ID3D12PipelineState> pipeline_state_;
-    Microsoft::WRL::ComPtr<ID3D12Fence> fence_;
-    std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, FrameCount> render_targets_;
-    HANDLE fence_event_{INVALID_HANDLE_VALUE};
-    UINT64 fence_value_{};
-    UINT rtv_descriptor_size_{};
-    D3D12_VIEWPORT viewport_{};
-    D3D12_RECT scissor_{};
+  Microsoft::WRL::ComPtr<ID3D12Device> device_;
+  Microsoft::WRL::ComPtr<ID3D12CommandQueue> command_queue_;
+  Microsoft::WRL::ComPtr<IDXGISwapChain3> swap_chain_;
+  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtv_heap_;
+  Microsoft::WRL::ComPtr<ID3D12CommandAllocator> command_allocator_;
+  Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> command_list_;
+  Microsoft::WRL::ComPtr<ID3D12RootSignature> root_signature_;
+  Microsoft::WRL::ComPtr<ID3D12PipelineState> pipeline_state_;
+  Microsoft::WRL::ComPtr<ID3D12Fence> fence_;
+  std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, FrameCount>
+      render_targets_;
+  HANDLE fence_event_{INVALID_HANDLE_VALUE};
+  UINT64 fence_value_{};
+  UINT rtv_descriptor_size_{};
+  D3D12_VIEWPORT viewport_{};
+  D3D12_RECT scissor_{};
 };
