3b362b491ceed1a755af1fdfd233bc48271aa900
Rômulo Henrique <66127263+Shiney-X@users.noreply.github.com>
2026-09-19 12:50:13 -0300

    build: mark sideload certificate for digital signing

--- scripts/windows/Build-UwpPackage.ps1
+++ scripts/windows/Build-UwpPackage.ps1
@@ -36,6 +36,7 @@ $certificate = New-SelfSignedCertificate `
     -KeyAlgorithm RSA `
     -KeyLength 2048 `
     -HashAlgorithm SHA256 `
+    -KeyUsage DigitalSignature `
     -KeyExportPolicy Exportable `
     -TextExtension @('2.5.29.37={text}1.3.6.1.5.5.7.3.3', '2.5.29.19={text}')
 
